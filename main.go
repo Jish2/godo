0169e34259d11870b20e38b9d5e5db23ddb00732
Joshua Goon <j.j.jappiecraft@gmail.com>
2024-07-21 17:30:23 -0700

    initial web server

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func getRoot(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Hello, World!\n")
+}
 
 func main() {
-	fmt.Println("Goodbye, World!")
+	http.HandleFunc("/", getRoot)
+
+	err := http.ListenAndServe(":3000", nil)
+
+	if errors.Is(err, http.ErrServerClosed) {
+		fmt.Printf("Server Closed\n")
+	} else if err != nil {
+		fmt.Printf("Error Starting Server: %s\n", err)
+		os.Exit(1)
+	}
 }
